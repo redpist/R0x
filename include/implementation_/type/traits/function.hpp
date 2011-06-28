e849f174d500fb3e8f00af801195ae40894707ef
Jeremy Lecerf aka Redpist <jeremy.lecerf@redpist.com>
2011-06-28 13:44:54 +0200

    typetraits added and iterators are now using it

---
+++ include/implementation_/type/traits/function.hpp
@@ -0,0 +1,63 @@
+//////////////////
+// Copyright (c) 2011, Jeremy Lecerf
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of R0x nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL JEREMY LECERF BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//////////////////
+
+#ifndef _R0X_TYPE_TRAITS_FUNCTION_H_
+#define _R0X_TYPE_TRAITS_FUNCTION_H_
+
+namespace R0x
+{
+  namespace Tools
+  {
+    namespace Type
+    {
+      namespace Traits
+      {
+        template <typename T>
+        struct Function;
+
+        template <typename R, typename... Args>
+        struct Function<R (*)(Args...)>
+        {
+          typedef R       ReturnType;
+        };
+
+        template <typename T>
+        struct MemberFunction;
+
+        template <typename R, class C, typename... Args>
+        struct MemberFunction<R (C::*)(Args...)>
+        {
+          typedef R     ReturnType;
+          typedef C     ClassType;
+        };
+
+      }
+    }
+  }
+}
+
+
+#endif /* _R0X_TYPE_TRAITS_FUNCTION_H_ */
