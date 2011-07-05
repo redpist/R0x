d46481a2118317739d0b1d30f695929b79ed1dc0
Jeremy Lecerf aka Redpist <jeremy.lecerf@redpist.com>
2011-07-05 14:22:11 +0200

    update test.sh

--- test.sh
+++ test.sh
@@ -9,15 +9,20 @@ fi
 for i in $params ; do
     echo "--------------------------------------------------------------------------------"
     echo "STARTING test ($i):"
+    rm -f "test/$i/_output"
     g++-4.6 -std=c++0x -Wextra -Wall -I include test/$i/main.cc && ./a.out > "test/$i/_output"
     if [ -f "test/$i/expected_output" ] ; then
-        output=`diff "test/$i/expected_output" "test/$i/_output"`
-        if [ -z "$output" ] ; then
-            result="OK"
+        if [ -f "test/$i/_output" ] ; then
+            output=`diff "test/$i/expected_output" "test/$i/_output"`
+            if [ -z "$output" ] ; then
+                result="OK"
+            else
+                result="ERROR"
+                echo "output differ from file \"test/$i/expected_output\""
+                echo -- "$output"
+            fi
         else
             result="ERROR"
-            echo "output differ from file \"test/$i/expected_output\""
-            echo -- "$output"
         fi
     else
         echo "no expected file : \"test/$i/expected_output\""
